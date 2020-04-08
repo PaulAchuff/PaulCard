017eed4eb3eba804913b2d68161b1ea191ecb799
Paul Achuff <paul.achuff@me.com>
2020-04-08 15:46:33 -0600

    Initial Commit

---
+++ PaulCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PaulCard
+//
+//  Created by Paul Achuff Sr on 4/8/20.
+//  Copyright © 2020 Paul Achuff Sr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
